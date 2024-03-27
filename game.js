7312c52fcf91d7dbc9c855eb716aea948d086d98
blaster003 <163987022+blaster003@users.noreply.github.com>
2024-03-27 01:33:18 +0000

    add automaclicker

--- game.js
+++ game.js
@@ -7,10 +7,22 @@ let lastTimestamp = 0;
 
 let mainPoints = 0;
 let mainPPS = 0;
+let automaclickers = 0;
+let automaCost = 20;
 
 function updateCounters() {
     document.getElementById("mainCounter").innerHTML = "This mentioned number is <h1>" + format(mainPoints) + "</h1>";
     document.getElementById("mainPPS").innerHTML = "It goes up by " + format(mainPPS) + " every second";
+    document.getElementById("automaBtn").innerHTML = mainPoints >= automaCost ? "Buy an Automaclicker" : "Buy an Automaclicker (you don't have enough)";
+    document.getElementById("automaCost").innerHTML = "Buying one would decrement " + format(automaCost) + " from your number.";
+}
+
+document.getElementById("automaBtn").onclick = function() {
+    if (mainPoints >= automaCost) {
+        mainPoints -= automaCost;
+        automaCost *= 1.2;
+        ++automaclickers;
+    }
 }
 
 function update(timestamp) {
@@ -18,7 +30,7 @@ function update(timestamp) {
     dt = (timestamp - lastTimestamp) / PERFECTFPS;
     lastTimestamp = timestamp;
 
-    // mainPoints += dt / 60;
+    mainPoints += (dt / 60) * automaclickers;
     updateCounters();
 
     rootElement.style.backgroundColor = "hsl(" + rootBGAnim + ", 32%, 68%)";
